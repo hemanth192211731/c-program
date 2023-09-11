4d214a504bd3525c36c395746a1fa1f90fc78c0d
hemanth192211731 <125177291+hemanth192211731@users.noreply.github.com>
2023-09-11 13:24:21 +0530

    Add files via upload

---
+++ simple intrest.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int p,t,r,c;
+	printf("enter the values of p,t,r");
+	scanf("%d%d%d",&p,&t,&r);
+	if(c=p*t*r/100);
+	printf("%d is simple intrest");
+	return 0;
+}
